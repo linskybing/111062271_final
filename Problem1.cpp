0fd99a1a579d8d36bce0355887423494ff255783
linskybing <linskybing@gmail.com>
2023-12-04 22:45:06 +0800

    1204

--- Problem1.cpp
+++ Problem1.cpp
@@ -1,16 +1,18 @@
 #include "basicDS.h"
+#include "Problem1.h"
+#include <queue>
+#include <utility>
 
 /* You can add more functions or variables in each class. 
    But you "Shall Not" delete any functions or variables that TAs defined. */
 
 class Problem1 {
-public:
-
-	Problem1(Graph G);  //constructor
-	~Problem1();        //destructor
-	void insert(int id, int s, Set D, int t, Graph &G, Tree &MTid);
-	void stop(int id, Graph &G, Forest &MTidForest);
-	void rearrange(Graph &G, Forest &MTidForest);
+	public:
+		Problem1(Graph G);  //constructor
+		~Problem1();        //destructor
+		void insert(int id, int s, Set D, int t, Graph &G, Tree &MTid);
+		void stop(int id, Graph &G, Forest &MTidForest);
+		void rearrange(Graph &G, Forest &MTidForest);
 };
 
 Problem1::Problem1(Graph G) {
@@ -24,9 +26,36 @@ Problem1::~Problem1() {
 }
 
 void Problem1::insert(int id, int s, Set D, int t, Graph &G, Tree &MTid) {
+
 	/* Store your output graph and multicast tree into G and MTid */
-	
+	Tree temp;
+	temp.s = s;
+	temp.id = id;
+	temp.ct = 0;
+
 	/* Write your code here. */
+
+	// disjoint set
+	DSU dsu(D.size);
+	priority_queue<graphEdge, vector<graphEdge>, Compare> pq;
+
+	//add all edge into priority queue
+	for (int i = 0; i < G.E.size(); i++) {
+		pq.push(G.E[i]);
+	}
+
+	int edge_need_num = D.size - 1;
+	while(!pq.empty() && edge_need_num) {
+		graphEdge g = pq.top();
+		pq.pop();
+
+		if (!dsu.same(g.vertex[0], g.vertex[1]) && g.b <= t) {
+			    dsu.uni(g.vertex[0], g.vertex[1]);
+                temp.ct += g.ce;
+                edge_need_num--;
+		}
+	}
+
 
 	return;
 }
---
+++ Problem1.h
@@ -0,0 +1,52 @@
+
+#ifndef PROBLEM1_H
+#define PROBLEM1_H
+#include <queue>
+
+class DSU {    
+    private:
+        int *p, *rank, s;
+    public:
+        DSU(int size) { 
+            s = size;
+            p = new int[size + 1];
+            rank = new int[size + 1];
+            reset();
+        }
+
+        ~DSU() {
+            delete[] p;
+            delete[] rank;
+        }
+
+        void reset() {
+            for (int i = 0; i <= s; i++) {
+                p[i] = i;
+                rank[i] = 1;
+            }
+        }
+        
+        int find(int a) {
+            if (a == p[a]) return a;
+            return p[a] = find(p[a]);
+        }
+        bool same(int a, int b) {
+            return find(a) == find(b);
+        }
+        void uni(int a, int b) {
+            if (same(a, b)) return;
+            int rA = rank[find(a)], rB = rank[find(b)];
+            if (rA > rB) { p[find(b)] = p[find(a)]; }
+            else if (rA == rB) { p[find(b)] = p[find(a)]; rank[p[find(a)]]++; }
+            else { p[find(a)] = p[find(b)]; }
+        }
+};
+
+struct Compare {
+    bool operator () (const struct graphEdge &lhs, const struct graphEdge &rhs) {
+        return lhs.ce > rhs.ce;
+    }
+};
+
+
+#endif
